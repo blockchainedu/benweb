7eb7c7f614937db12911c08b3ff76c8b0089a6f5
Oscar Lafarga <otech47@gmail.com>
2018-06-11 09:01:10 -0400

    Add typing animation to landing page tagline, closes #17

--- src/components/LandingTitle.jsx
+++ src/components/LandingTitle.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import Base from './Base';
+import AutomagicallyTypedTaglines from './AutomagicallyTypedTaglines';
 
 export default class LandingTitle extends Base {
     render() {
         return (
             <div className='LandingTitle'>
-                <h1 className='display-4 u-fw-600 text-white u-mb-40'>
-                    We build open, decentralized <span className='text-yellow' data-type='technology, communities'></span>
-                    <span className='typed-curcor text-yellow'>|</span>
-                </h1>
+                <div className='animated-tagline'>
+                    <h1>We build open, decentralized </h1>
+                    <AutomagicallyTypedTaglines
+                        taglines={['technology', 'communities']}
+                    />
+                </div>
                 <p className='description'>
                     The Blockchain Education Network is a 501(c)3 nonprofit dedicated to creating a world that is more open and free by providing a foundation for blockchain education and innovation.
                 </p>
